4770c6ab2a65d26a43058260f428d34d2227af9e
carlosfermed <harmonium_skies@hotmail.com>
2024-05-17 00:16:51 +0200

    first commit

---
+++ src/spotsaver/PreciosPorDia.java
@@ -0,0 +1,10 @@
+
+package spotsaver;
+
+public class PreciosPorDia {
+    // Precios por hora como constantes estáticas.
+    public static final double ARAGON = 14.90;
+    public static final double DIAGONAL = 17.90;
+    public static final double ARIBAU = 15.90;
+
+}
